77d766c7eeba9c00537d1c03ac8b2e2e1bee3c25
PrakashBabuD <prakashbabu.dontikurti@gmail.com>
2017-01-21 21:12:22 +1100

    Initial Commit

---
+++ SampleConsoleApplication/SampleConsoleApplication/Delegates.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApplication
+{
+    public delegate string MyDelegate (int a, int b);
+    class DelegatesDemo
+    {
+        public static string myFirstDelegate(int a ,int b)
+        {
+            return (a + b).ToString();
+        }
+
+        public static string mySecondDelegate(int a ,int b)
+        {
+            return (a * b).ToString();
+        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    MyDelegate del =DelegatesDemo.myFirstDelegate;
+        //    Console.WriteLine(del(2,3));
+        //    del = DelegatesDemo.mySecondDelegate;
+        //    MyDelegate del2 = delegate (int a, int b) {
+        //        return (a / b).ToString();
+        //    };
+        //    Console.WriteLine(del(2,3));
+        //    Console.WriteLine(del2(4,2));
+        //    Console.WriteLine("Composable Delegates Demo ");
+        //    ComposableDelegatesDemo cd =new ComposableDelegatesDemo();
+        //    cd.Demo();
+        //    Console.WriteLine("Composable Delegates Demo with reference");
+        //    ComposableDelegatesWithRefDemo cdr = new ComposableDelegatesWithRefDemo();
+        //    cdr.Demo();
+
+
+        //    Console.ReadLine();
+
+        //}
+    }
+    public delegate void MyComposableDelegate(int a, int b);
+    class ComposableDelegatesDemo
+    {
+        public static void  func1(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void func2(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+
+
+        public  void Demo()
+        {
+            
+            MyComposableDelegate f1 = func1;
+            MyComposableDelegate f2 = func2;
+            MyComposableDelegate f1f2 = f1 + f2;
+            f1f2(10, 10);
+        }
+    }
+    public delegate void MyComposableDelegateWithRef(int a, ref int b);
+
+    class ComposableDelegatesWithRefDemo
+    {
+        public static void func1(int a, ref int b)
+        {
+            Console.WriteLine(a + b);
+           b = b+2;
+        }
+
+        public static void func2(int a, ref int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+
+
+        public void Demo()
+        {
+            int b = 10;
+            int a = 10;
+            MyComposableDelegateWithRef f1 = func1;
+            MyComposableDelegateWithRef f2 = func2;
+            MyComposableDelegateWithRef f1f2 = f1 + f2;
+            f1f2(a, ref b);
+
+
+
+
+
+
+
+        }
+    }
+
+}
---
+++ SampleConsoleApplication/SampleConsoleApplication/EventsDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Security.Policy;
+using Microsoft.SqlServer.Server;
+
+namespace SampleConsoleApplication {
+
+    
+    public delegate void  VideoEncoderHandler(object source,EventArgs args);
+
+    public class EventsDemo
+    {
+        public static void Main(string[] args)
+        {
+            Video video=new Video();
+            VideoEncoder videoEncoder=new VideoEncoder();
+            MailSender mailSender =new MailSender();
+            videoEncoder.VideEncoderEvent += mailSender.OnVideoEncodedEvent;
+            videoEncoder.Encode(video);
+            Console.ReadLine();
+
+        }
+
+    }
+
+
+    public class Video
+    {
+        public string VideoName { get; set; }
+
+    }
+
+    public class VideoEncoder
+    {
+        public event VideoEncoderHandler VideEncoderEvent ;
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Enconding  is completed ");
+            OnVideEncodedEvent();
+
+        }
+
+        public void OnVideEncodedEvent()
+        {
+            if (VideEncoderEvent!=null)
+            {
+                VideEncoderEvent(this,EventArgs.Empty);
+            }
+        }
+
+
+    }
+
+    public class MailSender
+    {
+        public void OnVideoEncodedEvent(Object source,EventArgs args)
+        {
+            Console.WriteLine("Mail Sent Successfully");
+        }
+    }
+
+
+
+
+}
